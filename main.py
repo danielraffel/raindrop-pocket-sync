38977c41abb5a627e10e2895bc3ac5ebc5e5068c
daniel raffel <daniel.raffel@gmail.com>
2025-04-06 00:49:42 -0700

    s

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ POCKET_ACCESS_TOKEN = os.getenv("POCKET_ACCESS_TOKEN")
 RAINDROP_COLLECTION_ID = os.getenv("RAINDROP_COLLECTION_ID", "0")
 
 DB_PATH = "/opt/raindrop-pocket-sync/db.sqlite3"
-RAINDROP_API = "https://api.raindrop.io/rest/v1/raindrop"
+RAINDROP_API = "https://api.raindrop.io/rest/v1/raindrops"
 POCKET_ADD_API = "https://getpocket.com/v3/add"
 POCKET_SEND_API = "https://getpocket.com/v3/send"
 
